4011dff803c67ca7511307cf5c35bf3f8a577f52
Gerard Iglesias <gerard_iglesias@me.com>
2019-09-21 20:24:53 +0200

    Added an AppKit of version

---
+++ HCal/HCal/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HCal
+//
+//  Created by Gerard Iglesias on 16/09/2019.
+//  Copyright © 2019 Gerard Iglesias. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
